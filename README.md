003c83281fcca6694847ac86688229ed58ce0c3f
DanielC19 <danysam1010@gmail.com>
2025-05-04 22:48:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# NewtonLab Proyecto de análisis numérico
+
+## Estudiantes
+- Santiago Acevedo Urrego
+- Miguel Ángel Cano Salinas
+- Daniel Correa Botero
+
+## Descripción de la aplicación
+Los métodos que nuestra aplicación resuelve son:
+- Método de Bisección
+- Método de Regla Falsa
+- Método de Punto Fijo
+- Método de Newton-Raphson
+- Método de la Secante
+- Método de Raíces Múltiples #1
+- Método de Raíces Múltiples #2
+- Método de Jacobi
+- Método de Gauss-Seidel
+- Método SOR (Sobre-relajación Sucesiva)
+- Método de Vandermonde
+- Método de Interpolación de Newton
+- Método de Lagrange
+- Métodos de Spline Lineal y Cúbico
+
+## Requisitos previos
+- Python 3.x instalado.
+- `pip` instalado.
+- Virtualenv (opcional pero recomendado).
+
+## Pasos para configurar el proyecto
+
+1. **Crear un entorno virtual (opcional pero recomendado)**
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # En Windows: venv\Scripts\activate
+   ```
+
+2. **Instalar dependencias**
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. **Crear variables de entorno**
+   - Copiar el archivo `.env.example` a `.env`:
+     ```bash
+     cp .env.example .env
+     ```
+   - Modificar el archivo `.env` con las configuraciones apropiadas según sea necesario.
+
+4. **Ejecutar el servidor**
+   ```bash
+   python manage.py runserver
+   ```
+
+5. **Acceder a la aplicación**
+   - Abrir el navegador y visitar: [http://127.0.0.1:8000/](http://127.0.0.1:8000/).
---
+++ config/settings.py
@@ -0,0 +1,72 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+INSTALLED_APPS = [
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "src.application.numerical_method",
+    "src.application.home",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "src/application/shared/templates",
+            BASE_DIR / "src/application/home/templates",
+            BASE_DIR / "src/application/numerical_method/templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.dummy",
+    }
+}
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
