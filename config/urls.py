21055b162e30b44c045eeafb1e8eedad8a0ded83
DanielC19 <danysam1010@gmail.com>
2025-05-21 10:31:17 -0500

    Feature: Add templates and visual components

--- config/urls.py
+++ config/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("src.application.home.urls")),
+    path("numerical-methods/", include("src.application.numerical_method.urls")),
 ]
