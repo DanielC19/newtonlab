96960f04ee0a9576e7b93571a86b1cd661020ff0
M4C4NO5 <139782321+M4C4NO5@users.noreply.github.com>
2025-05-22 10:18:32 -0500

    Fix: Launching our app

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
